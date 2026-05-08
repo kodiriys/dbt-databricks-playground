6f5cf0edbaa4c4645a1c4d92debbfea922d3f4f6
kodiriys <45722718+kodiriys@users.noreply.github.com>
2026-05-07 17:55:11 -0700

    Updated notebooks for stage and prod environments

--- Notebooks/ingest_csv_claim_data.py
+++
@@ -1,8 +0,0 @@
-# Can optionally do some basic pre-processing and cleanup
-df = spark.table("workspace.raw_sftp.claims")
-df_selected = df.select(
-    "claim_id", "date_of_loss", "body_part", "is_litigated", "number_of_priors"
-).filter((df.claim_id.isNotNull()) & (df.claim_id != ""))
-df_selected.write.format("delta").option("overwriteSchema", "true").mode(
-    "overwrite"
-).saveAsTable("medallion.0_landing.claims")
